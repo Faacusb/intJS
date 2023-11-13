886d84fed8fa2339c9c1dc14681d1ff252cbc1d6
facundo <faacuusb@hotmail.com>
2023-11-13 16:26:26 -0300

    menuhamjs check

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const iconMenu = document.querySelector('.iconoMenuHamb'),
  menu = document.querySelector('.navbar-list');
 
+ 
+
+
+ const toggle_menu = ()=>{
+        menu.classList.toggle('toggleMenu')
+        console.log('holi')
+ }
+
+
+const init = ()=> {
+
+
+    
+iconMenu.addEventListener('click',toggle_menu)
+
 console.log(menu)
-
-iconMenu.addEventListener('click', (e => {
-    menu.classList.toggle('toggleMenu')
-}))
-
-console.log(iconMenu)
+}
+init()
